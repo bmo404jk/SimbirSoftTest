2280c0004d4d9a9302b33761e7c437a9b3ccf0f9
bmo404jk <78249495+bmo404jk@users.noreply.github.com>
2024-04-11 05:37:54 +0400

    Delete SimbirSoftTest directory

--- SimbirSoftTest/src/test/java/Base/BaseSeleniumPage.java
+++
@@ -1,9 +0,0 @@
-package Base;
-import org.openqa.selenium.WebDriver;
-abstract public class BaseSeleniumPage {
-    protected static WebDriver driver;
-    public static void setDriver(WebDriver webDriver) {
-
-        driver = webDriver;
-    }
-}
